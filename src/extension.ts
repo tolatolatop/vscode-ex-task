b4ff2281f8bad5af7616a295710bb3cf885d6f03
tolatolatop <tolatolatop@gmail.com>
2025-06-19 22:11:37 +0000

    feat: 支持显示资源

--- src/extension.ts
+++ src/extension.ts
@@ -12,24 +12,85 @@ import {
 	TreeViewTaskItem
 } from './taskTypes';
 
+// 资源项类
+class ResourceItem extends vscode.TreeItem {
+	constructor(
+		public readonly resourceUriString: string
+	) {
+		super(
+			path.basename(resourceUriString),
+			vscode.TreeItemCollapsibleState.None
+		);
+
+		this.tooltip = resourceUriString;
+		this.description = resourceUriString;
+		this.iconPath = new vscode.ThemeIcon('file');
+		this.resourceUri = vscode.Uri.parse(resourceUriString);
+
+		// 设置命令：点击资源项时打开文件
+		this.command = {
+			command: 'vscode.open',
+			title: '打开资源',
+			arguments: [this.resourceUri]
+		};
+	}
+}
+
+// 任务项类
+class TaskItem extends vscode.TreeItem {
+	constructor(
+		public readonly task: TaskDefinition
+	) {
+		super(
+			task.label,
+			// 如果有资源，则设置为可展开，否则为不可展开
+			(task.resource && task.resource.length > 0)
+				? vscode.TreeItemCollapsibleState.Collapsed
+				: vscode.TreeItemCollapsibleState.None
+		);
+
+		// 设置工具提示
+		this.tooltip = `${task.label} - ${task.command} ${task.args?.join(' ') || ''}`;
+
+		// 根据任务状态设置图标
+		if (task.id === -1) {
+			this.iconPath = new vscode.ThemeIcon('clock', new vscode.ThemeColor('warningForeground'));
+			this.contextValue = 'pendingTask';
+		} else {
+			this.iconPath = new vscode.ThemeIcon('check', new vscode.ThemeColor('charts.green'));
+			this.contextValue = 'submittedTask';
+		}
+
+		// 不设置description，保持简洁
+		this.description = '';
+	}
+}
+
 // 任务数据提供者类
-class TaskTreeDataProvider implements vscode.TreeDataProvider<TaskItem> {
-	private _onDidChangeTreeData: vscode.EventEmitter<TaskItem | undefined | null | void> = new vscode.EventEmitter<TaskItem | undefined | null | void>();
-	readonly onDidChangeTreeData: vscode.Event<TaskItem | undefined | null | void> = this._onDidChangeTreeData.event;
+class TaskTreeDataProvider implements vscode.TreeDataProvider<TaskItem | ResourceItem> {
+	private _onDidChangeTreeData: vscode.EventEmitter<TaskItem | ResourceItem | undefined | null | void> = new vscode.EventEmitter<TaskItem | ResourceItem | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<TaskItem | ResourceItem | undefined | null | void> = this._onDidChangeTreeData.event;
 
 	refresh(): void {
 		this._onDidChangeTreeData.fire();
 	}
 
-	getTreeItem(element: TaskItem): vscode.TreeItem {
+	getTreeItem(element: TaskItem | ResourceItem): vscode.TreeItem {
 		return element;
 	}
 
-	getChildren(element?: TaskItem): Thenable<TaskItem[]> {
-		if (element) {
+	getChildren(element?: TaskItem | ResourceItem): Thenable<TaskItem[] | ResourceItem[]> {
+		if (element instanceof ResourceItem) {
+			// 资源项没有子节点
 			return Promise.resolve([]);
+		} else if (element instanceof TaskItem) {
+			// 任务项的子节点是资源项
+			const resources = element.task.resource || [];
+			return Promise.resolve(resources.map(uri => new ResourceItem(uri)));
+		} else {
+			// 根节点返回任务列表
+			return this.getTasks();
 		}
-		return this.getTasks();
 	}
 
 	private async getTasks(): Promise<TaskItem[]> {
@@ -54,33 +115,6 @@ class TaskTreeDataProvider implements vscode.TreeDataProvider<TaskItem> {
 			console.error('读取任务配置失败:', error);
 			return [];
 		}
-	}
-}
-
-// 任务项类
-class TaskItem extends vscode.TreeItem {
-	constructor(
-		public readonly task: TaskDefinition
-	) {
-		super(
-			task.label,
-			vscode.TreeItemCollapsibleState.None
-		);
-
-		// 设置工具提示
-		this.tooltip = `${task.label} - ${task.command} ${task.args?.join(' ') || ''}`;
-
-		// 根据任务状态设置图标
-		if (task.id === -1) {
-			this.iconPath = new vscode.ThemeIcon('clock', new vscode.ThemeColor('warningForeground'));
-			this.contextValue = 'pendingTask';
-		} else {
-			this.iconPath = new vscode.ThemeIcon('check', new vscode.ThemeColor('charts.green'));
-			this.contextValue = 'submittedTask';
-		}
-
-		// 不设置description，保持简洁
-		this.description = '';
 	}
 }
 
